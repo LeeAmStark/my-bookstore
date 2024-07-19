9ca58b26185a650f1f6c17ee1800843899118e45
LeeAmStark <williamsigbinoba2@gmail.com>
2024-07-19 16:05:23 +0100

    Frontend set - for back-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+require('dotenv').config();
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
@@ -11,23 +13,38 @@ import About from "./components/About";
 import Cart from "./components/shared/Cart";
 import Order from "./components/Order";
 
-function App() {
+const apiUrl = process.env.REACT_APP_URL;
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}/api/data`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error making the request!", error);
+      });
+  }, []);
+
   return (
     <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Signup />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<Order />} />
-        </Routes>
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
